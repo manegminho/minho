df39e55b6093df04eb3f589ce36471031efc7bb6
manegminho <46932195+manegminho@users.noreply.github.com>
2019-01-24 01:23:55 +0900

    Add files via upload

--- calculation.js
+++ calculation.js
@@ -2,11 +2,15 @@ function fnAllOutputgoogoodan(num)
 {
 	var ndigit = num;
 	var result;
+	var nColor = 000000 + (200*num);
+	document.write("<font size =5 color = 0000FF>" +"<b>" + ndigit+" 단 구구단 "
+		+"<br>" + "</b>"+"<hr>"+"</font>");
+
 	
 	for(var j=1;j<10;j++)
 	{
 		result = ndigit * j;
-		document.write( "<br>" + ndigit +" X " + j + "=" + result);
+		document.write(ndigit +" X " + j + "=" + result + "<br>" + "</font>");
 	}
 	document.write("<hr>" );
 }
